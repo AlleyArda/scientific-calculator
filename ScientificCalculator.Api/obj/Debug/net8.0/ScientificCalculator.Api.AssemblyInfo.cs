11ce30bf489b83be915164a144b503c3f5421169
Ali Arda Kulaksız <92370387+AlleyArda@users.noreply.github.com>
2025-01-07 16:00:06 +0300

    Screenshots added
    projects front-end ui screenshots added

--- ScientificCalculator.Api/obj/Debug/net8.0/ScientificCalculator.Api.AssemblyInfo.cs
+++ ScientificCalculator.Api/obj/Debug/net8.0/ScientificCalculator.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ScientificCalculator.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fa73e3e0dccb27a1a6952377d3bb42576a67384")]
 [assembly: System.Reflection.AssemblyProductAttribute("ScientificCalculator.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ScientificCalculator.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
